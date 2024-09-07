7c831945b79662a64593725fbf8c0a42d830140d
Mert Türedü <62012483+AzmanMerto@users.noreply.github.com>
2024-09-07 05:03:47 +0300

    xcode file init

---
+++ CanvasProjectApp/CanvasProjectApp/CanvasProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasProjectAppApp.swift
+//  CanvasProjectApp
+//
+//  Created by Mert Türedü on 7.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CanvasProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
